052b370d063f4b81d20ad40f7aa6bc0c5dff7be3
jopaulo666 <jp_cbc@hotmail.com>
2021-07-09 00:00:26 -0300

    Integrando o front-end com o back-end

---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,11 @@
+class Stock {
+    id: number;
+    name: string;
+    price: number;
+    minimum: number;
+    exclusiveMinimum: boolean;
+    variation: number;
+    data: string;
+}
+
+export default Stock;
